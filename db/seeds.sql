e10b26701eb5b21fe1ee8a96827ec6157d968954
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-13 23:56:27 -0700

    added a db folder and inside added a schema.sql and seeds.sql file with my burger data to import into mysql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Single Hamburger');
+INSERT INTO burgers (name) VALUES ('Single Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Grassfed Cheesburger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheesburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
