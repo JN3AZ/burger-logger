54e947ac3e3b5018852f382809fe44c7f4c1af57
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-15 22:47:31 -0700

    fixed seeds.sql to have burger_name instead of name, built my connection.js file with the setup for mysql to connect, also set up my orm.js file with the import connection setup as well built the three methods needed to retrieve and store the data inside burger_db gave it the proper export, also added the burger.js file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (name) VALUES ('Single Hamburger');
-INSERT INTO burgers (name) VALUES ('Single Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Grassfed Cheesburger');
-INSERT INTO burgers (name) VALUES ('Bacon Cheesburger');
-INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Single Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Single Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Grassfed Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
