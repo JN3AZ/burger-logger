2f66999c1ca25a11c59f3a7d2c7d79c946526a60
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-17 00:42:50 -0700

    set up the jawsDB connection for heroku in my connection.js file

--- config/connection.js
+++ config/connection.js
@@ -17,16 +17,17 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } 
-// else {
-//   connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: ''
-//   });
-// };
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} 
+else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: ''
+  });
+};
+
 // Export connection for our ORM to use.
 module.exports = connection;
