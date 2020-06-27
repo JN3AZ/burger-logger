7a21479f267be5707048f8b08f3b1d9d0c75ba06
Morpheu$ <64348796+JN3AZ@users.noreply.github.com>
2020-06-26 21:47:50 -0700

    edited connection configuration in conection.js

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,35 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL) {
+  // connection to database on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  // connection = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "hacktheplanet",
+  //   database: "todoagain_db",
+  // });
+  // connection to local database
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+}
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
   }
   console.log("connected as id " + connection.threadId);
 });
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} 
-else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
-  });
-};
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
